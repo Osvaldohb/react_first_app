87d5b3458b56c469e4b39e26013f13225176272d
Osvaldohb <osvaldonuevo@gmail.com>
2022-10-12 11:57:30 -0500

    Firs commit

---
+++ src/context/TaksContext.jsx
@@ -0,0 +1,37 @@
+import { tasks as data } from "../data/task";
+import { createContext, useState,useEffect } from "react";
+export const TasksContext = createContext();
+
+export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TasksContext.Provider
+      value={{
+        tasks,
+        deleteTask,
+        createTask,
+      }}
+    >
+      {props.children}
+    </TasksContext.Provider>
+  );
+}
